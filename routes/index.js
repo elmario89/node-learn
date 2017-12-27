1ee722ce6307ae3f9b938b372683a3fd0b3f913b
Михаил Смирнов <urine89@mail.ru>
2017-12-27 22:04:48 +0300

    initial

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+// Do work here
+router.get('/', (req, res) => {
+    res.render('hello', {title: 'I love food'});
+});
+
+router.get('/reverse/:name', (req, res) => {
+    const reverse = [...req.params.name].reverse().join('');
+    // res.send(reverse);
+})
+
+module.exports = router;
