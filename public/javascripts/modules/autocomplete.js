cdfc5db09d88779ca57bcf888ac1d416dc88f344
Михаил Смирнов <urine89@mail.ru>
2017-12-30 03:08:35 +0300

    Lng and lat added on city choose

---
+++ public/javascripts/modules/autocomplete.js
@@ -0,0 +1,29 @@
+import {$$} from './bling';
+
+function autocomplete(input, latInput, lngInput, image) {
+    if (!input) return;
+
+    const dropdown = new google.maps.places.Autocomplete(input);
+
+    dropdown.addListener('place_changed', () => {
+        const place = dropdown.getPlace();
+
+        if (!place.photos) return;
+        place.photos.forEach((item, index) => renderImages(place.photos[index].getUrl({maxWidth: 250}),image));
+
+        latInput.value = place.geometry.location.lat();
+        lngInput.value = place.geometry.location.lng();
+    });
+
+    input.on('keydown', (e) => {
+        if (e.keyCode === 13) e.preventDefault();
+    });
+}
+
+function renderImages(url, container) {
+    const imageEl = document.createElement('img');
+    imageEl.src = url;
+    container.appendChild(imageEl);
+}
+
+export default autocomplete;
